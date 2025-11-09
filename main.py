65a52b0078f8b0b6a4bd41551c428e3c4903b452
Oriol Agost Batalla <github@agost.info>
2025-11-09 12:09:51 +0100

    feat: add dependencies, main api structure

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from cloud-template!")
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
+app = FastAPI(
+    title="Sample user API",
+    description="API for managing user data",
+    version="0.1.0",
+)
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root() -> RedirectResponse:
+    return RedirectResponse(url="/docs")
