76c76b442386afba2bc3d64a71f623ecef435e4a
Oriol Agost Batalla <github@agost.info>
2025-11-09 13:36:04 +0100

    feat: allow server to be run from main

--- main.py
+++ main.py
@@ -25,3 +25,7 @@ app.include_router(user.router)
 @app.get("/")
 async def root() -> RedirectResponse:
     return RedirectResponse(url="/docs")
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
